af767a7798c7e49a889551ec948ae97625d20c2e
Vladimir Popov <schubert0603@gmail.com>
2023-03-28 15:10:03 +0300

    Update main.py
    Changed color recognizing

--- src/main.py
+++ src/main.py
@@ -4,20 +4,20 @@ import numpy as np
 import cv2
 from IPython.display import display
 import matplotlib
+from PIL import Image
 
 
 data_set_path = r'data\color_names.csv' 
 data_set = pd.read_csv(data_set_path)
 img_path = r'etc\test_img.jpg' 
-img = cv2.imread(img_path, 1)
+img = Image.open(img_path).convert("RGB")
 display(data_set)
 
-#FIXME
-cv2.imshow('image', img)
-color_point = img[300,300]
+r, g ,b = img.getpixel((5,5))
 
-print(matplotlib.colors.to_hex(color_point))
 
+a = (r,g,b)
+display(a)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
