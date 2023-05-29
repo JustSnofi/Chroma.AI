9e1ac99778ab4bf4c06873f7f6e17dead3981382
Adam <adamartzi5136@gmail.com>
2023-05-29 14:48:50 +0300

    updated userdata

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,12 @@ from tkinter import filedialog
 from PIL import ImageTk, Image
 import numpy as np
 import pathlib
+import os
+from os import path
+import shelve
+import userdata as ud
+
+username = os.getlogin()
 
 class Application(tk.Frame):
     '''GUI Application'''
@@ -13,19 +19,30 @@ class Application(tk.Frame):
         self.master.title("ChromAi - color blind helper")
         icon_image = tk.PhotoImage(file="img\icon.png")
         self.master.iconphoto(True, icon_image)
+        self.option_var = tk.StringVar(self)
+
+        self.blindness_types = ('Normal', 
+                                'Deuteranopia', 
+                                'Tritanopia', 
+                                'Monochromacy')
+        
         self.pack()
         self.create_widgets()
     
     def create_widgets(self):
-        self.select_file_button = tk.Button(self, text="Select Image", command=self.select_file)
-        self.select_file_button.pack()
+        self.select_file_button = tk.Button(self, text="Select Image", command=self.select_file).pack()
         
-        self.color_label = tk.Label(self, text="Color in Center Pixel: ")
-        self.color_label.pack()
+        self.color_label = tk.Label(self, text="Color in Center Pixel: ").pack()
         
-        self.image_label = tk.Label(self)
-        self.image_label.pack()
-    
+        self.image_label = tk.Label(self).pack()
+
+        self.select_blindness = tk.OptionMenu(self, self.option_var,*self.blindness_types, command=self.get_blindness).pack()
+
+        self.save_userdata_button = tk.Button(self, 'Save your Userdata', command=self.make_userdata).pack()
+
+        if not path.exists(f'.userdata/{username}'):
+            self.load_userdata_button = tk.Button(self, text='Load your Userdata', command=self.load_userdata).pack()
+
     def select_file(self):
         global file_path
         file_path = filedialog.askopenfilename(filetypes=[("PNG files", "*.png"), ('JPEG files', "*.jpg"), ('JPEG files', "*.jpeg")])
@@ -42,6 +59,28 @@ class Application(tk.Frame):
             self.image_label.image = img_tk
             # make_user_data()
             return file_path
+    
+    def get_blindness(self, *args):
+        return self.option_var.get()
+    
+
+    '''User Data functions (make/load)'''
+    def make_userdata(self):
+        global data
+        data = shelve.open(f'.userdata/{username}')
+        data['file_path'] = file_path
+        data['file_ext'] = 'jpg'
+        data['blindness_type'] = blindness_type
+        data.close()
+
+    def load_userdata():
+        file_path = data['file_path']
+        file_ext = data['file_ext']
+        blindness_type = data['blindness_type']
+        data.close()
+
+        return (file_path, file_ext, blindness_type)
+    
 
 def get_file_path() -> str:
     '''Returns file_path'''
@@ -56,8 +95,6 @@ def get_file_extension() -> str:
 def make_user_data():
     with open('.userdata\data.csv', 'w') as f:
         f.write(file_path + '/n' + file_extension) 
-
-
 
     
 root = tk.Tk()
--- src/userdata.py
+++ src/userdata.py
@@ -1,5 +1,25 @@
 import shelve
+import os
+from os import path
 
-# TODO
-def make_userdata(file_path, file_ext):
-    pass
+username = os.getlogin()
+
+def make(file_path, file_ext, blindness_type):
+    global data
+    data = shelve.open(username)
+    data['file_path'] = file_path
+    data['file_ext'] = file_ext
+    data['blindness_type'] = blindness_type
+    data.close()
+
+def load():
+    file_path = data['file_path']
+    file_ext = data['file_ext']
+    blindness_type = data['blindness_type']
+    data.close()
+    
+    return (file_path, file_ext, blindness_type)
+
+#if path.exists(f'.userdata/{username}'):
+    
+    
